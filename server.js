acc1ac4abc09d83e76d1043ba66e0f2af9f00b27
Josh <josh@vanessas-MacBook-Air.local>
2026-07-20 14:50:44 -0400

    Add /api/teams route to fetch league data

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const db = require('./database.js');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('BLS App Engine is Live! ');
+});
+
+app.get('/api/teams', (req, res) => {
+  const teams = db.prepare('SELECT * FROM teams').all();
+  res.json(teams);
 });
 
 app.listen(PORT, () => {
